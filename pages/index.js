82b4a732dfc18aa347be44d89bb635a2bcf3671a
Alaina Koerber <alaina.koerber@prismic.io>
2022-12-14 13:44:05 +0100

    SEO

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import { SliceZone, PrismicRichText } from '@prismicio/react';
 import { Layout } from '../components/Layout';
 import { createClient } from '../prismicio';
 import { components } from '../slices';
+import Head from 'next/head';
 
 const Page = ({ page, navigation }) => {
   return (
     <Layout navigation={navigation}>
+      <Head>
+      <span>{ page.data.seo_title }</span>
+      <span>{ page.data.seo_description }</span>
+      </Head>
       <div> 
         {/* <PrismicRichText field={page.data.title}/> */}
         <div className='mega-gum-logo-wrap'><img className='mega-gum-logo' src={page.data.homeimage.url} alt={page.data.homeimage.alt} /></div>
